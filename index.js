85750d7551fbd760ef16ebb6beaabb6337ec547d
Benjamin Netter <benjamin.netter@gmail.com>
2015-04-07 23:10:37 +0200

    Now handling _pre and compiled error messages

--- index.js
+++ index.js
@@ -3,13 +3,85 @@ var async = require('async');
 
 var messiah = {};
 
+/**
+ * Load rules methods and a set of default rules
+ */
+
 messiah.rules = require(__dirname + '/rules.js');
+
+/**
+ * Load object methods
+ */
+
 messiah.objects = require(__dirname + '/objects.js');
+
+/**
+ * Load default options
+ */
+
 messiah.options = require(__dirname + '/options.js');
+
+/**
+ * Compiles the message, aggregating variables from the object
+ */
+
+messiah._compileErrors = function (errors, object) {
+  if (errors) {
+    if (_.isArray(errors)) {
+      return _.map(errors, function (error) {
+        var message  = _.template(error);
+
+        return message(object);
+      });
+    } else {
+      _.forEach(_.keys(errors), function (key) {
+        errors[key] = _.map(errors[key], function (error) {
+          var message  = _.template(error);
+
+          return message(object);
+        });
+      });
+    }
+  }
+
+  return errors;
+};
+
+/**
+ * Checks the property and returns the errors
+ */
+
+messiah._checkValue = function (value, rules, context, callback) {
+  var err = [];
+
+  if ((value === null || _.isUndefined(value)) && !_.isUndefined(rules.isRequired)) {
+    err.push(rules.isRequired.message);
+  }
+
+  async.each(_.keys(rules), function (rule, cb) {
+    if (rule !== 'isRequired') {
+      var options = rules[rule];
+      var validator = messiah.rules._get(rule);
+
+      return validator(value, options, context, function(result) {
+        if (!result) {
+          err.push(options.message);
+        }
+
+        return cb(null);
+      });
+    }
+
+    return cb(null);
+  }, function() {
+    return callback(err);
+  });
+};
 
 messiah.check = function (type, object, context, next) {
   var errors = {};
 
+  // `context` is optional
   if (_.isFunction(context)) {
     next = context;
     context = null;
@@ -17,35 +89,23 @@ messiah.check = function (type, object, context, next) {
 
   var properties = messiah.objects._get(type);
 
-  async.eachSeries(_.keys(properties), function (property, callback) {
-    var value = object[property];
+  async.eachSeries(_.keys(properties), function(property, callback) {
     var rules = properties[property];
-    var err = [];
+    var value = object[property];
 
-    if ((value === null || _.isUndefined(value)) && !_.isUndefined(rules.isRequired)) {
-      err.push(rules.isRequired.message);
+    if (property === '_pre') {
+      value = object;
     }
 
-    async.each(_.keys(rules), function (rule, cb) {
-      if (rule !== 'isRequired') {
-        var options = rules[rule];
-        var validator = messiah.rules._get(rule);
-
-        return validator(value, options, context, function(result) {
-          if (!result) {
-            err.push(options.message);
-          }
-
-          return cb(null);
-        });
-      }
-
-      return cb(null);
-    }, function() {
+    return messiah._checkValue(value, rules, context, function(err) {
       if (err.length > 0) {
-        errors[property] = err;
+        if (property === '_pre') {
+          errors = err;
+        } else {
+          errors[property] = err;
+        }
 
-        if (messiah.options.stopped) {
+        if (messiah.options.breaks || property === '_pre') {
           return callback(true); // stops the current execution right away
         }
       }
@@ -57,7 +117,7 @@ messiah.check = function (type, object, context, next) {
       errors = null;
     }
 
-    return next(errors);
+    return next(messiah._compileErrors(errors, object));
   });
 };
 
